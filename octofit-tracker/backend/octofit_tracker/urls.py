c99f1fd77edd7bb4e25431a0b0a5fed75ce01e0d
Gianni Mastroscianni <giannisoad12@gmail.com>
2025-12-01 14:38:24 +0000

    Rimuovi credenziali di accesso dal file di configurazione del database e aggiungi import per l'amministrazione Django

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.contrib import admin
 from rest_framework import routers
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet
 from rest_framework.response import Response
